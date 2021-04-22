053058e5bcf7cba850596e52d69c418ecc488ae8
ngocchau0112 <ngocchau0112@gmail.com>
2021-04-23 02:53:30 +0700

    Added rel="noreferrer" to link

--- src/Photos.js
+++ src/Photos.js
@@ -7,7 +7,7 @@ export default function Photos(props) {
         {props.photos.map(function (photo, index) {
           return (
             <div className="col photo" key={index}>
-              <a href={photo.src.original} target="_blank">
+              <a href={photo.src.original} target="_blank" rel="noreferrer">
                 <img
                   src={photo.src.landscape}
                   className="img-fluid"
