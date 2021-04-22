eaabbb2dfe5d9e6fd5c27c71f254a84bda9b3123
ngocchau0112 <ngocchau0112@gmail.com>
2021-04-22 23:51:56 +0700

    Added word's meanings

---
+++ src/Meaning.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+export default function Meaning(props) {
+  console.log(props);
+  return (
+    <div className="meaning">
+      <h3>{props.meaning.partOfSpeech}</h3>
+      {props.meaning.definitions.map(function (definition, index) {
+        return (
+          <div className="" key={index}>
+            <p>
+              {definition.definition}
+              <br />
+              <em>{definition.example}</em>
+            </p>
+          </div>
+        );
+      })}
+    </div>
+  );
+}
--- src/Results.js
+++ src/Results.js
@@ -1,5 +1,24 @@
 import React from "react";
+import Meaning from "./Meaning";
 
-export default function Results(response) {
-  return <div className="container">Hello</div>;
+export default function Results(props) {
+  console.log(props);
+  if (props.results) {
+    return (
+      <div className="container">
+        <div className="results">
+          <h2>{props.results.word}</h2>
+          {props.results.meanings.map(function (meaning, index) {
+            return (
+              <div key={index}>
+                <Meaning meaning={meaning} />
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  } else {
+    return null;
+  }
 }
